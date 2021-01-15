55a65c7629ad9ee1083f6c98b2386f18f34bf41b
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-15 20:37:18 +0100

    Update admin password for phpipam

--- linux/software/phpipam/generic/configure_database.sh
+++ linux/software/phpipam/generic/configure_database.sh
@@ -11,12 +11,12 @@ mariadb_password=$3
 database_name=$4
 database_user=$5
 database_password=$6
+application_password=$7
+phpipam_install_path=$8
 
-phpipam_install_path=$7
-
-mysql -u $mariadb_username -p$mariadb_password -e "CREATE DATABASE $database_name;"
-mysql -u $mariadb_username -p$mariadb_password -e "GRANT ALL ON $database_name.* TO $database_user@localhost IDENTIFIED BY '$database_password';"
-mysql -u $mariadb_username -p$mariadb_password -e "FLUSH PRIVILEGES;"
+mysql -h $mariadb_hostname -u $mariadb_username -p$mariadb_password -e "CREATE DATABASE $database_name;"
+mysql -h $mariadb_hostname -u $mariadb_username -p$mariadb_password -e "GRANT ALL ON $database_name.* TO $database_user@localhost IDENTIFIED BY '$database_password';"
+mysql -h $mariadb_hostname -u $mariadb_username -p$mariadb_password -e "FLUSH PRIVILEGES;"
 
 
 sed -i "s/db\['host'\] = 'localhost'/db\['host'\] = '$mariadb_hostname'/" $phpipam_install_path/config.php
@@ -26,6 +26,9 @@ sed -i "s/db\['name'\] = 'phpipam'/db\['name'\] = '$database_name'/" $phpipam_in
 
 mysql -u $mariadb_username -p$mariadb_password $database_name < $phpipam_install_path/db/SCHEMA.sql
 
+php $phpipam_install_path/functions/scripts/reset-admin-password.php $application_password
+
+mysql -h $mariadb_hostname -u $mariadb_username -p$mariadb_password $mariadb_hostname -e "UPDATE $database_name.users SET passChange='No'WHERE id=1;"
 
 echo "#                                                                      #"
 echo "#                                                                      #"
