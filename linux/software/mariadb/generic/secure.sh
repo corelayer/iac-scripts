31c7e6c5df7df962f34964bf2f05b9094d34c3e8
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-15 16:19:43 +0100

    Update secure.sh

--- linux/software/mariadb/generic/secure.sh
+++ linux/software/mariadb/generic/secure.sh
@@ -7,11 +7,11 @@ echo "#                                                                      #"
 
 mysql_root_password=$1
 
-mysql -e "UPDATE mysql.user SET Password=PASSWORD($mysql_root_password) WHERE User='root';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DROP DATABASE test;"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD($mysql_root_password) WHERE User='root';"
+mysql -u root -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+mysql -u root -e "DELETE FROM mysql.user WHERE User='';"
+mysql -u root -e "DROP DATABASE test;"
+mysql -u root -e "FLUSH PRIVILEGES;"
 
 
 echo "#                                                                      #"
