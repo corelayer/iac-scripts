385722c3b1166cdaba58dddb28cbff98d59d1e64
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-20 20:51:03 +0100

    Teamcity 20.2.1 - Base install

--- linux/software/jetbrains/teamcity/generic/install-20.2.1.sh
+++ linux/software/jetbrains/teamcity/generic/install-20.2.1.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 echo "########################################################################"
-echo "LINUX - SOFTWARE - POSTGRESQL - Configure"
+echo "LINUX - SOFTWARE - JETBRAINS - TEAMCITY - GENERIC - Install"
 echo "########################################################################"
 echo "#                                                                      #"
 echo "#                                                                      #"
 
 
-ipv4_listen=$1
-
-echo "### Configure postgresql to listen on address"
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '$ipv4_listen'/" /etc/postgresql/12/main/postgresql.conf
-
-echo "### Configure client authentication for external clients"
-echo 'host    all      all              0.0.0.0/0                    md5' | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
-echo 'host    all      all              ::/0                         md5' | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
-
+cd /tmp
+wget https://download.jetbrains.com/teamcity/TeamCity-2020.2.1.tar.gz
+tar zxvf TeamCity*
 
 echo "#                                                                      #"
 echo "#                                                                      #"
