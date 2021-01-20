16a9b0556e0c856ce0dbf30cc110230162be5639
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-20 22:29:58 +0100

    TeamCity

--- linux/software/jetbrains/teamcity/generic/install-2020.2.1.sh
+++ linux/software/jetbrains/teamcity/generic/install-2020.2.1.sh
@@ -7,7 +7,6 @@ echo "#                                                                      #"
 
 
 target_install_path=$1
-target_data_path=$2
 
 ### Fetch version 2020.2.1 from JetBrains
 cd /tmp
@@ -15,12 +14,9 @@ wget https://download.jetbrains.com/teamcity/TeamCity-2020.2.1.tar.gz
 tar zxvf TeamCity*
 mv /tmp/TeamCity $target_install_path
 
-### Create data directory for TeamCity
-mkdir -p $target_data_path
-
-## Set ownership and permissions
-chown -R teamcity:root /var/teamcity*
-chmod -R 770 /var/teamcity*
+#### Set ownership and permissions
+chown -R teamcity:root $target_install_path
+chmod -R 770 $target_install_path
 
 
 echo "#                                                                      #"
