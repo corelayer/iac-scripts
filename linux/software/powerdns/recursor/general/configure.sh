f6a6c5c094b8c9c81695611b3f582df98cf8333f
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-22 10:42:41 +0100

    PowerDNS Recursor

--- linux/software/powerdns/recursor/general/configure.sh
+++ linux/software/powerdns/recursor/general/configure.sh
@@ -5,8 +5,10 @@ echo "########################################################################"
 echo "#                                                                      #"
 echo "#                                                                      #"
 
-ipaddress=$1
-cp ./linux/software/powerdns/recursor/templates/recursor.conf /etc/powerdns/.
+template_path=$1
+ipaddress=$2
+
+cp $template_path/linux/software/powerdns/recursor/templates/recursor.conf /etc/powerdns/.
 sed -i "s/local-address=0.0.0.0/local-address=$ipaddress/" /etc/powerdns/recursor.conf
 
 
