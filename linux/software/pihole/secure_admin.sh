d186696ae41c4a8cd354e6c888b6ddba98eb148a
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-22 09:43:30 +0100

    Update security configuration pi-hole

--- linux/software/pihole/secure_admin.sh
+++ linux/software/pihole/secure_admin.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 echo "########################################################################"
-echo "LINUX - SOFTWARE - PI-HOLE - Configure"
+echo "LINUX - SOFTWARE - PI-HOLE - Secure Admin panel"
 echo "########################################################################"
 echo "#                                                                      #"
 echo "#                                                                      #"
 
 
-ipv4_listen=$1
-upstream_dns1=$2
-upstream_dns2=$3
-
-cp -r templates/* /etc/pihole/.
-
-sed -i "s/IPV4_ADDRESS=0.0.0.0/IPV4_ADDRESS=$ipv4_listen/" /etc/pihole/setupVars.conf
-sed -i "s/PIHOLE_DNS_1=0.0.0.0/PIHOLE_DNS_1=$upstream_dns1/" /etc/pihole/setupVars.conf
-sed -i "s/PIHOLE_DNS_2=0.0.0.0/PIHOLE_DNS_2=$upstream_dns1/" /etc/pihole/setupVars.conf
+webpassword=$1
+pihole -a -p $webpassword
 
 
 echo "#                                                                      #"
