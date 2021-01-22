5edaf20e1bd3111c3512a002a0004b8179133d62
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-22 09:07:43 +0100

    Pihole default web password

--- linux/software/pihole/configure.sh
+++ linux/software/pihole/configure.sh
@@ -16,7 +16,7 @@ cp -r templates/* /etc/pihole/.
 sed -i "s/IPV4_ADDRESS=0.0.0.0/IPV4_ADDRESS=$ipv4_listen/" /etc/pihole/setupVars.conf
 sed -i "s/PIHOLE_DNS_1=0.0.0.0/PIHOLE_DNS_1=$upstream_dns1/" /etc/pihole/setupVars.conf
 sed -i "s/PIHOLE_DNS_2=0.0.0.0/PIHOLE_DNS_2=$upstream_dns1/" /etc/pihole/setupVars.conf
-sed -i "s/WEBPASSWORD=/WEBPASSWORD=$webpassword/" /etc/pihole/setupVars.conf
+sed -i "s/WEBPASSWORD=000000000000000000000000/WEBPASSWORD=$webpassword/" /etc/pihole/setupVars.conf
 
 
 echo "#                                                                      #"
