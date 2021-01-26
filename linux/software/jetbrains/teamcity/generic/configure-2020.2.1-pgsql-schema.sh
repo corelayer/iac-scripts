b3e8cbb511158bd3a26735cf55728d5ba392cd4e
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-01-26 15:04:20 +0100

    Update configure-2020.2.1-pgsql-schema.sh

--- linux/software/jetbrains/teamcity/generic/configure-2020.2.1-pgsql-schema.sh
+++ linux/software/jetbrains/teamcity/generic/configure-2020.2.1-pgsql-schema.sh
@@ -10,6 +10,7 @@ database_user=$2
 database_password=$3
 database_schema=$4
 
+su -c "createdb $database_name" postgres
 su -c "psql -c \"CREATE ROLE $database_user LOGIN PASSWORD '$database_password' NOINHERIT CREATEDB;\" -d $database_name -U postgres" postgres
 su -c "psql -c \"CREATE SCHEMA IF NOT EXISTS $database_schema AUTHORIZATION $database_user;\" -d $database_name -U postgres" postgres
 
