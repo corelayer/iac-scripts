55aede3ed78f083a9aac00f0f1676ebec7f89b54
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-02-03 16:41:27 +0100

    Missing quotes

--- linux/software/pihole/database/install_firebog.sh
+++ linux/software/pihole/database/install_firebog.sh
@@ -12,8 +12,10 @@ wget -O $inputFile https://v.firebog.net/hosts/lists.php?type=tick
 inputLines=$(cat $inputFile)
 for line in $inputLines
 do
-    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled) VALUES($line, 1);"
+    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled) VALUES('$line', 1);"
 done
+pihole -g
+
 
 echo "#                                                                      #"
 echo "#                                                                      #"
