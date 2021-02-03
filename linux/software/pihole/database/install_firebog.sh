2196f44c5b93952d71b7d74b2e45f8dd27a06eef
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-02-03 16:36:44 +0100

    Updated pihole scripts

---
+++ linux/software/pihole/database/install_firebog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - PI-HOLE - Install Firebog lists"
+echo "########################################################################"
+echo "#                                                                      #"
+echo "#                                                                      #"
+
+
+inputFile=/tmp/firebog.txt
+wget -O $inputFile https://v.firebog.net/hosts/lists.php?type=tick
+
+inputLines=$(cat $inputFile)
+for line in $inputLines
+do
+    sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (address, enabled) VALUES($line, 1);"
+done
+
+echo "#                                                                      #"
+echo "#                                                                      #"
+echo "########################################################################"
+echo " "
+echo " "
+echo " "
+echo " "
