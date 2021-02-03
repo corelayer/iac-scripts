24cf5705a3e60b6ca27b0985f6a7b5aab88481ca
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-02-03 16:44:57 +0100

    Create install_whitelists.sh

---
+++ linux/software/pihole/database/install_whitelists.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - PI-HOLE - Install Whitelist entries"
+echo "########################################################################"
+echo "#                                                                      #"
+echo "#                                                                      #"
+
+
+pihole -w "sp.nba.com"
+pihole -w "go.sdworx.com"
+pihole -w "go.fortinet.com"
+
+
+echo "#                                                                      #"
+echo "#                                                                      #"
+echo "########################################################################"
+echo " "
+echo " "
+echo " "
+echo " "
