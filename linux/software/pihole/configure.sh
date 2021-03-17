12bc74b49e6249e8b01acc43ee9af3cfd9913de9
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-03-17 16:05:31 +0100

    Add automatic netplan apply to pihole on reboot

--- linux/software/pihole/configure.sh
+++ linux/software/pihole/configure.sh
@@ -15,6 +15,9 @@ sed -i "s/PIHOLE_DNS_1=0.0.0.0/PIHOLE_DNS_1=$upstream_dns1/" /etc/pihole/setupVa
 sed -i "s/PIHOLE_DNS_2=0.0.0.0/PIHOLE_DNS_2=$upstream_dns2/" /etc/pihole/setupVars.conf
 
 
+apply_line="@reboot netplan apply"
+echo $apply_line | crontab -u root -
+
 echo "#                                                                      #"
 echo "#                                                                      #"
 echo "########################################################################"
