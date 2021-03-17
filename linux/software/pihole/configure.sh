7d32363ff10b32aed2da2b1af7e26601ed468c34
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-03-17 16:28:37 +0100

    Comment

--- linux/software/pihole/configure.sh
+++ linux/software/pihole/configure.sh
@@ -15,6 +15,8 @@ sed -i "s/PIHOLE_DNS_1=0.0.0.0/PIHOLE_DNS_1=$upstream_dns1/" /etc/pihole/setupVa
 sed -i "s/PIHOLE_DNS_2=0.0.0.0/PIHOLE_DNS_2=$upstream_dns2/" /etc/pihole/setupVars.conf
 
 
+# Pi-Hole gets a second IP address messing up the routing table
+# By enforcing a "netplan apply" every minute, the issue is solved
 apply_line="* * * * * /usr/sbin/netplan apply"
 echo $apply_line | crontab -u root -
 
