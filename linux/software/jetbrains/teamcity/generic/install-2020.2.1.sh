6457445c337731467be1511827569bf278cf2d38
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-03-18 10:13:47 +0100

    TeamCity autostart conditional

--- linux/software/jetbrains/teamcity/generic/install-2020.2.1.sh
+++ linux/software/jetbrains/teamcity/generic/install-2020.2.1.sh
@@ -7,6 +7,7 @@ echo "#                                                                      #"
 
 
 target_install_path=$1
+auto_start_on_boot=$2
 
 ### Fetch version 2020.2.1 from JetBrains
 cd /tmp
@@ -17,6 +18,13 @@ mv /tmp/TeamCity $target_install_path
 #### Set ownership and permissions
 chown -R teamcity:root $target_install_path
 chmod -R 770 $target_install_path
+
+if [ "$auto_start_on_boot" = true ]; then
+    # Pi-Hole gets a second IP address messing up the routing table
+    # By enforcing a "netplan apply" every minute, the issue is solved
+    apply_line="@reboot /var/teamcity/bin/runAll.sh start"
+    echo $apply_line | crontab -u teamcity -
+fi
 
 
 echo "#                                                                      #"
