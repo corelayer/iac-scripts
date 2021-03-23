729aa02134e92d6ad81c7dea76662b6ab87aeeed
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-03-23 10:13:27 +0100

    Add basepath

--- linux/software/privacyidea/privacyidea-freeradius/generic/configure_rlm.sh
+++ linux/software/privacyidea/privacyidea-freeradius/generic/configure_rlm.sh
@@ -5,8 +5,9 @@ echo "########################################################################"
 echo "#                                                                      #"
 echo "#                                                                      #"
 
-fqdn=$1
-check_ssl=$2
+template_basepath=$1
+fqdn=$2
+check_ssl=$3
 
 cp $template_basepath/linux/software/privacyidea/privacyidea-freeradius/templates/etc/privacyidea/rlm.ini /etc/privacyidea/.
 sed -i "s~fqdn~$fqdn~" /etc/privacyidea/rlm_perl.ini
