cb251335037ef61aa1b09aea8720a750421ca59f
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-03-23 09:39:14 +0100

    PrivacyIdea
    Repository + installer freeradius plugin

---
+++ linux/software/privacyidea/privacyidea-freeradius/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - PRIVACYIDEA - GENERAL - REPOSITORY Ubuntu"
+echo "########################################################################"
+echo "#                                                                      #"
+echo "#                                                                      #"
+
+
+apt-get install privacyidea-radius
+
+echo "#                                                                      #"
+echo "#                                                                      #"
+echo "########################################################################"
+echo " "
+echo " "
+echo " "
+echo " "
