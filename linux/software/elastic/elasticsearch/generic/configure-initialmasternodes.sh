dc3a09ea59e2a2ee157539b2f9cd42cfe6b4b3b4
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-10-19 16:47:20 +0200

    sed command update

--- linux/software/elastic/elasticsearch/generic/configure-initialmasternodes.sh
+++ linux/software/elastic/elasticsearch/generic/configure-initialmasternodes.sh
@@ -9,7 +9,7 @@ echo "#                                                                      #"
 filepath=$1
 master_nodes=$2
 echo $master_nodes
-sed -i "s~#cluster.initial_master_nodes: [\"node-1\", \"node-2\"]~cluster.initial_master_nodes: $master_nodes~" $filepath
+sed -i "s~#cluster.initial_master_nodes: \[\"node-1\", \"node-2\"\]~cluster.initial_master_nodes: $master_nodes~" $filepath
 
 echo "#                                                                      #"
 echo "#                                                                      #"
