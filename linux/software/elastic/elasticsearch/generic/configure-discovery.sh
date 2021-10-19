2af8e4a18aad856c6d453b615215147ceeda5efb
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-10-19 16:05:24 +0200

    Debug logging

--- linux/software/elastic/elasticsearch/generic/configure-discovery.sh
+++ linux/software/elastic/elasticsearch/generic/configure-discovery.sh
@@ -8,6 +8,7 @@ echo "#                                                                      #"
 
 filepath=$1
 seed_hosts=$2
+echo $seed_hosts
 
 sed -i "s~#discovery.seed_hosts: [\"host1\", \"host2\"]~discovery.seed_hosts: $seed_hosts~" $filepath
 
