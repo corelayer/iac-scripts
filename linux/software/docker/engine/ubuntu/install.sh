48eba78922e741e96815ea6bfb5670a8bbadea03
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-10-27 17:51:55 +0200

    Updates for Elastic / FortinetVPN

---
+++ linux/software/docker/engine/ubuntu/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - DOCKER - ENGINE - Install"
+echo "########################################################################"
+echo "#"
+
+echo "### Install Docker Engine"
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo "# - DONE"
+echo "########################################################################"
+echo " "
