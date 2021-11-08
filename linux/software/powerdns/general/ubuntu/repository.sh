62842c62d1a97529ee8a7045f7edb60ad30d5195
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-11-08 11:18:13 +0100

    Updates

--- linux/software/powerdns/general/ubuntu/repository.sh
+++ linux/software/powerdns/general/ubuntu/repository.sh
@@ -5,7 +5,7 @@ echo "########################################################################"
 echo "#                                                                      #"
 echo "#                                                                      #"
 
-
+echo "###"
 curl https://repo.powerdns.com/FD380FBB-pub.asc | apt-key add -
 add-apt-repository "deb [arch=amd64] http://repo.powerdns.com/ubuntu focal-rec-44 main"
 apt-get update
