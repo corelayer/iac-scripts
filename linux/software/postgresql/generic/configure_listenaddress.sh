ad9a730989c45dd51cc0504d1f599792b0ae29d5
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-11-08 18:08:34 +0100

    TimescaleDB

--- linux/software/postgresql/generic/configure_listenaddress.sh
+++ linux/software/postgresql/generic/configure_listenaddress.sh
@@ -15,6 +15,7 @@ echo "### Configure client authentication for external clients"
 echo 'host    all      all              0.0.0.0/0                    md5' | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
 echo 'host    all      all              ::/0                         md5' | sudo tee -a /etc/postgresql/12/main/pg_hba.conf
 
+service postgresql restart
 
 echo "#                                                                      #"
 echo "#                                                                      #"
---
+++ linux/software/timescaledb/ubuntu/install_12.sh
@@ -0,0 +1,2 @@
+# Now install appropriate package for PG version
+sudo apt install timescaledb-2-postgresql-13
