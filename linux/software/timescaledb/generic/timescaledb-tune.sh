6390b5d6aa392a3f35917dbdd2874ea21b8e05f4
Jan Tytgat <jan.tytgat@corelayer.eu>
2021-11-09 09:29:50 +0100

    TimeScaleDB 14.x and Tuning

---
+++ linux/software/timescaledb/generic/timescaledb-tune.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - TimeScaleDB - Generic - Tune"
+echo "########################################################################"
+echo "#                                                                      #"
+echo "#                                                                      #"
+
+echo "### Tune TimeScaleDB for PostgreSQL"
+timescaledb-tune --quiet --yes
+service postgresql restart
+
+echo "#                                                                      #"
+echo "########################################################################"
+echo " "
---
+++ linux/software/timescaledb/ubuntu/install_14.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "########################################################################"
+echo "LINUX - SOFTWARE - TimeScaleDB - Ubuntu - Install for PostgreSQL 14.x"
+echo "########################################################################"
+echo "#                                                                      #"
+echo "#                                                                      #"
+
+echo "### Install TimeScaleDB for PostgreSQL 14.x"
+sudo apt install timescaledb-2-postgresql-14
+
+echo "#                                                                      #"
+echo "########################################################################"
+echo " "
+# Now install appropriate package for PG version
