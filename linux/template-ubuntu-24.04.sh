bd340f5f0e6363c2ef8d0807e674391df0e57ed0
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-08-12 11:23:36 +0200

    Update filename and update comments in template script
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- linux/template-ubuntu-24.04.sh
+++ linux/template-ubuntu-24.04.sh
@@ -3,14 +3,17 @@ echo "########################################################################"
 echo "UBUNTU TEMPLATE FROM MINIMAL INSTALLATION"
 echo "########################################################################"
 echo "### Update system"
+echo "------------------------------------------------------------------------"
 sh /tmp/iac-scripts/linux/os/distro/deb-based/generic/apt/apt-update.sh
 sh /tmp/iac-scripts/linux/os/distro/deb-based/generic/apt/apt-upgrade.sh
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 echo "### Configure package sources"
 echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 echo "### Install additional packages"
+echo "------------------------------------------------------------------------"
 sh /tmp/iac-scripts/linux/software/apparmor/apt-install_apparmor-profiles.sh
 sh /tmp/iac-scripts/linux/software/apparmor/apt-install_apparmor-utils.sh
 sh /tmp/iac-scripts/linux/software/cronie/apt-install.sh
@@ -21,13 +24,15 @@ sh /tmp/iac-scripts/linux/software/itop/apt-install.sh
 sh /tmp/iac-scripts/linux/software/net-tools/apt-install.sh
 sh /tmp/iac-scripts/linux/software/screen/apt-install.sh
 sh /tmp/iac-scripts/linux/software/traceroute/apt-install.sh
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 echo "### Execute template base configuration scripts"
-sh /tmp/iac-scripts/linux/os/generic/grub/net-ifnames.sh
 echo "------------------------------------------------------------------------"
+sh /tmp/iac-scripts/linux/os/generic/grub/net-ifnames.sh
+echo "########################################################################"
 
 echo "### Execute template hardening scripts"
+echo "------------------------------------------------------------------------"
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/apparmor/enforce-ping.sh
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/apparmor/enforce-traceroute.sh
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/etc/fail2ban.sh
@@ -39,19 +44,20 @@ sh /tmp/iac-scripts/linux/os/generic/security/hardening/etc/modprobe.sh
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/etc/motd.sh
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/etc/sysctl.sh
 sh /tmp/iac-scripts/linux/os/generic/security/hardening/ssh/sshd_config.sh
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 echo "### Template cleanup"
+echo "------------------------------------------------------------------------"
 sh /tmp/iac-scripts/linux/os/generic/crontab/cleanup-user.sh root
 sh /tmp/iac-scripts/linux/os/generic/crontab/cleanup-user.sh $PROVISIONER_USERNAME
 sh /tmp/iac-scripts/linux/os/generic/cloud-init/cleanup.sh
 sh /tmp/iac-scripts/linux/os/distro/deb-based/generic/apt/apt-autoremove.sh
 sh /tmp/iac-scripts/linux/os/distro/deb-based/generic/apt/apt-autoclean.sh
-echo "------------------------------------------------------------------------"
+echo "########################################################################"
 
 echo "### Remove Provisioner User"
-sh /tmp/iac-scripts/linux/os/generic/security/users/remove_provisioner_user.sh $PROVISIONER_USERNAME
 echo "------------------------------------------------------------------------"
+sh /tmp/iac-scripts/linux/os/generic/security/users/remove_provisioner_user.sh $PROVISIONER_USERNAME
 
 echo "########################################################################"
 echo " "
