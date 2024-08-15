b459456a479858ddc3badda562cfe55389df4ae1
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-08-15 23:24:45 +0200

    update wireguard scripts
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

---
+++ linux/software/wireguard/install-scripts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git clone https://github.com/corelayer/wireguard-scripts /tmp/wireguard-scripts
+sh /tmp/wireguard-scripts/install.sh
--- linux/software/wireguard/install-scripts_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/corelayer/wireguard-scripts /tmp/wireguard-scripts
-cp -R /tmp/wireguard-scripts/server/* /etc/wireguard/.
-
-
-chown -R root:root /etc/wireguard/*
-chmod -R 600 /etc/wireguard/*
