e7698a6961b019bb687a5a5586139c1f0a4c1d8a
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-09-05 17:41:56 +0200

    Update zabbix scripts
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- linux/software/zabbix/ubuntu-24-04/apt-configure-7.0.sh
+++ linux/software/zabbix/ubuntu-24-04/apt-configure-7.0.sh
@@ -5,8 +5,8 @@ echo "### --> install dependencies"
 DEBIAN_FRONTEND="noninteractive" apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 echo "### --> add repository"
-wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-2+ubuntu22.04_all.deb
-dpkg -i zabbix-release_7.0-2+ubuntu22.04_all.deb
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-2+ubuntu24.04_all.deb
+dpkg -i zabbix-release_7.0-2+ubuntu24.04_all.deb
 
 echo "### --> update package list"
 apt-get update
