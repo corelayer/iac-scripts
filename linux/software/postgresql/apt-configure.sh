02f6c70bf65007e8f8662dbea0cd70a6d72e6efe
Jan Tytgat <jan.tytgat@corelayer.eu>
2024-09-05 14:10:40 +0200

    Update postgresql
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- linux/software/postgresql/apt-configure.sh
+++ linux/software/postgresql/apt-configure.sh
@@ -5,10 +5,11 @@ echo "### --> install dependencies"
 DEBIAN_FRONTEND="noninteractive" apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 echo "### --> add repository key"
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O- | apt-key add -
+install -d /usr/share/postgresql-common/pgdg
+curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 
 echo "### --> add repository to /etc/apt/sources.list.d/postgresql.list"
-echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list
+echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/postgresql.list
 
 echo "### --> update package list"
 apt-get update
--- linux/software/postgresql/apt-install_postgresql-14.sh
+++ linux/software/postgresql/apt-install_postgresql-14.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "### install postgresql-12"
-DEBIAN_FRONTEND="noninteractive" apt-get install -y postgresql-12
+echo "### install postgresql-14"
+DEBIAN_FRONTEND="noninteractive" apt-get install -y postgresql-14
