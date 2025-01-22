0bfefe1e60e3e65f3821cf8b778e6278b80af164
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-01-22 15:40:29 +0100

    update caddy

--- linux/software/caddy/apt-configure.sh
+++ linux/software/caddy/apt-configure.sh
@@ -2,12 +2,12 @@
 echo "### configure repository for caddy"
 
 
-sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl
+apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl
 
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | \
- sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+ gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | \
- sudo tee /etc/apt/sources.list.d/caddy-stable.list
+ tee /etc/apt/sources.list.d/caddy-stable.list
 
-sudo apt-get update
+apt-get update
