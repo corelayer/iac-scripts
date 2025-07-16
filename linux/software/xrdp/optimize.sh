2a4ffb5c8c7f9150591aa1bfda9e098bcc55f409
Jan Tytgat <jan.tytgat@corelayer.eu>
2025-07-16 15:00:10 +0200

    fix optimize.sh for xrdp
    Signed-off-by: Jan Tytgat <jan.tytgat@corelayer.eu>

--- linux/software/xrdp/optimize.sh
+++ linux/software/xrdp/optimize.sh
@@ -2,4 +2,4 @@
 echo "### optimize xrdp"
 cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
 
-sed s/#tcp_send_buffer_bytes=32768/tcp_send_buffer_bytes=4194304/ </etc/xrdp/xrdp.ini >/etc/xrdp/xrdp.ini
+sed -i 's/#tcp_send_buffer_bytes=32768/tcp_send_buffer_bytes=4194304/g' /etc/xrdp/xrdp.ini
